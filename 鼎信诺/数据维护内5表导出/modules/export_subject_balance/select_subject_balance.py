484636898621263d3081a46e8667f4a1ee8223fb
noel <151730075+inxi-noel@users.noreply.github.com>
2026-03-18 14:45:48 +0800

    Delete 鼎信诺/数据维护内5表导出 directory

--- 鼎信诺/数据维护内5表导出/modules/export_subject_balance/select_subject_balance.py
+++
@@ -1,8 +0,0 @@
-from modules._shared.select_table_by_index import select_table_by_index
-
-
-def select_subject_balance():
-    return select_table_by_index(
-        target_index=0,
-        expected_text="科目余额表(基本表)"
-    )
